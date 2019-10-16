2f3b736f56e27487f1df0e8f1b8d04ce09aa0d97
deczen <deczen@gmail.com>
2019-10-16 08:25:53 +0700

    Version 2.0.0.66
    Fixes:
    - Fix auto update function

--- custom/updater/Plugin-Update.php
+++ custom/updater/Plugin-Update.php
@@ -1,8 +1,4 @@
 <?php
-global $pagenow;
-
-if ( is_admin() && $pagenow==='plugins.php' ) {	//only do in plugins.php page
-	require_once( 'BFIGitHubPluginUploader.php' );
-    new BFIGitHubPluginUpdater( ZIPPERAGENTPATH . '/zipperagent.php', 'deczen', "za" );
-}
+require_once( 'BFIGitHubPluginUploader.php' );
+new BFIGitHubPluginUpdater( ZIPPERAGENTPATH . '/zipperagent.php', 'deczen', "za" );
 ?>
